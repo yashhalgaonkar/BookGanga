4c7e262e21b0fc63e0264ab1635315a7f4f3472b
Yash Halgaonkar <yash.halgaonkar18@vit.edu>
2021-05-22 15:51:42 +0530

    22/05: Added Indexed Stack to nav screen

--- lib/ui/screens/nav_screen.dart
+++ lib/ui/screens/nav_screen.dart
@@ -1,8 +1,6 @@
 import 'package:book_ganga/config/book_ganga.dart';
 import 'package:book_ganga/ui/screens/screens.dart';
-import 'package:book_ganga/ui/widgets/profile_avatar.dart';
 import 'package:flutter/material.dart';
-import 'package:line_icons/line_icons.dart';
 import '../../data/data.dart';
 import 'HomeScreen/home_screen.dart';
 import 'package:ionicons/ionicons.dart';
@@ -39,103 +37,95 @@ class _NavScreenState extends State<NavScreen> {
     Ionicons.person_outline,
   ];
 
-  int _selectedIndex = 0;
+  ValueNotifier<int> _pageNumberNotifier = ValueNotifier<int>(0);
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageNumberNotifier.dispose();
+  }
+
+  void _onItemTapped(int index) {
+    _pageNumberNotifier.value = index;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _screens[_selectedIndex],
-      // bottomNavigationBar: BottomNavigationBar(
-      //   type: BottomNavigationBarType.fixed,
-      //   selectedItemColor: BookGanga.kAccentColor,
-      //   //unselectedItemColor: Colors.black87,
-      //   backgroundColor: Colors.white,
-      //   currentIndex: _selectedIndex,
-      //   onTap: (value) {
-      //     setState(() {
-      //       _selectedIndex = value;
-      //     });
-      //   },
-      //   items: [
-      //     BottomNavigationBarItem(
-      //         icon: Icon(
-      //             (_selectedIndex == 0) ? filledIcons[0] : outline_icons[0]),
-      //         label: "Home"),
-      //     BottomNavigationBarItem(
-      //         icon: Icon(
-      //             (_selectedIndex == 1) ? filledIcons[1] : outline_icons[1]),
-      //         label: "Books"),
-      //     BottomNavigationBarItem(
-      //         icon: Icon(
-      //             (_selectedIndex == 2) ? filledIcons[2] : outline_icons[2]),
-      //         label: "Write"),
-      //     BottomNavigationBarItem(
-      //         icon: Icon(
-      //             (_selectedIndex == 3) ? filledIcons[3] : outline_icons[3]),
-      //         label: "Activity"),
-      //     BottomNavigationBarItem(
-      //       icon: CircleAvatar(
-      //         radius: 14,
-      //         backgroundImage: AssetImage("assets/images/dp.jpeg"),
-      //       ),
-      //       label: "Profile",
-      //     ),
-      //   ],
-      // ),
-      bottomNavigationBar: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 6.0),
-        child: GNav(
-            selectedIndex: _selectedIndex,
-            onTabChange: (index) {
-              setState(() {
-                _selectedIndex = index;
-              });
-            },
-            rippleColor:
-                Colors.purple[800], // tab button ripple color when pressed
-            hoverColor: Colors.purple[700], // tab button hover color
-            haptic: false, // haptic feedback
-            tabBorderRadius: 15,
-            // tabActiveBorder:
-            //     Border.all(color: Colors.black, width: 1), // tab button border
-            // tabBorder:
-            //     Border.all(color: Colors.grey, width: 1), // tab button border
-            // tabShadow: [
-            //   BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8)
-            // ], // tab button shadow
-            curve: Curves.easeIn, // tab animation curves
-            duration: Duration(milliseconds: 100), // tab animation duration
-            gap: 8, // the tab button gap between icon and text
-            color: Colors.grey[800], // unselected icon color
-            activeColor: BookGanga.kAccentColor, // selected icon and text color
-            iconSize: 24, // tab button icon size
-            tabBackgroundColor:
-                Colors.purple.withOpacity(0.1), // selected tab background color
-            padding: EdgeInsets.symmetric(
-                vertical: 10, horizontal: 3), // navigation bar padding
-            tabs: [
-              GButton(
-                icon: (_selectedIndex == 0) ? filledIcons[0] : outline_icons[0],
-                text: 'Home',
-              ),
-              GButton(
-                icon: (_selectedIndex == 1) ? filledIcons[1] : outline_icons[1],
-                text: 'Books',
-              ),
-              GButton(
-                icon: (_selectedIndex == 2) ? filledIcons[2] : outline_icons[2],
-                text: 'Write',
-              ),
-              GButton(
-                icon: (_selectedIndex == 3) ? filledIcons[3] : outline_icons[3],
-                text: 'Activity',
-              ),
-              GButton(
-                icon: (_selectedIndex == 4) ? filledIcons[4] : outline_icons[4],
-                text: 'Profile',
-              )
-            ]),
+    return SafeArea(
+      child: ValueListenableBuilder(
+        valueListenable: _pageNumberNotifier,
+        builder: (context, pageNumber, Widget child) {
+          return Scaffold(
+            body: IndexedStack(
+              children: _screens,
+              index: _pageNumberNotifier.value,
+            ),
+            bottomNavigationBar: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 6.0),
+              child: GNav(
+                  selectedIndex: pageNumber,
+                  onTabChange: _onItemTapped,
+                  rippleColor: Colors
+                      .purple[800], // tab button ripple color when pressed
+                  hoverColor: Colors.purple[700], // tab button hover color
+                  haptic: false, // haptic feedback
+                  tabBorderRadius: 15,
+                  // tabActiveBorder:
+                  //     Border.all(color: Colors.black, width: 1), // tab button border
+                  // tabBorder:
+                  //     Border.all(color: Colors.grey, width: 1), // tab button border
+                  // tabShadow: [
+                  //   BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8)
+                  // ], // tab button shadow
+                  curve: Curves.easeIn, // tab animation curves
+                  duration:
+                      Duration(milliseconds: 100), // tab animation duration
+                  gap: 8, // the tab button gap between icon and text
+                  color: Colors.grey[800], // unselected icon color
+                  activeColor:
+                      BookGanga.kAccentColor, // selected icon and text color
+                  iconSize: 24, // tab button icon size
+                  tabBackgroundColor: Colors.purple
+                      .withOpacity(0.1), // selected tab background color
+                  padding: EdgeInsets.symmetric(
+                      vertical: 10, horizontal: 3), // navigation bar padding
+                  tabs: [
+                    GButton(
+                      icon: (pageNumber == 0)
+                          ? filledIcons[0]
+                          : outline_icons[0],
+                      text: 'Home',
+                    ),
+                    GButton(
+                      icon: (pageNumber == 1)
+                          ? filledIcons[1]
+                          : outline_icons[1],
+                      text: 'Books',
+                    ),
+                    GButton(
+                      icon: (pageNumber == 2)
+                          ? filledIcons[2]
+                          : outline_icons[2],
+                      text: 'Write',
+                    ),
+                    GButton(
+                      icon: (pageNumber == 3)
+                          ? filledIcons[3]
+                          : outline_icons[3],
+                      text: 'Activity',
+                    ),
+                    GButton(
+                      icon: (pageNumber == 4)
+                          ? filledIcons[4]
+                          : outline_icons[4],
+                      text: 'Profile',
+                    )
+                  ]),
+            ),
+          );
+        },
       ),
+
     );
   }
 }
