6f5f55b66c648b23d9b849f8063ee0bc48f05e02
Yash Halgaonkar <yash.halgaonkar18@vit.edu>
2021-06-02 12:38:38 +0530

    Completed the UI of Community Space

--- lib/ui/screens/BookClub/chat_list_tab.dart
+++ lib/ui/screens/BookClub/chat_list_tab.dart
@@ -1,10 +1,34 @@
+import 'package:book_ganga/data/data.dart';
+import 'package:book_ganga/ui/widgets/my_input_field.dart';
+import 'package:book_ganga/ui/widgets/profile_avatar.dart';
 import 'package:flutter/material.dart';
 
 class ChatList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Chay List View'),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+          child: MyInputField(hintText: 'Search here..'),
+        ),
+        Expanded(
+          child: Container(
+            margin: const EdgeInsets.all(10.0),
+            child: ListView.builder(
+              itemCount: dummyUser.length,
+              itemBuilder: (BuildContext context, int index) {
+                final user = dummyUser[index];
+                return ListTile(
+                  leading: ProfileAvatar(imageUrl: user.profileImageUrl),
+                  title: Text('${user.fname} ${user.lname}'),
+                  subtitle: Text('@${user.username}'),
+                );
+              },
+            ),
+          ),
+        )
+      ],
     );
   }
 }
--- lib/ui/screens/BookClub/club_list_tab.dart
+++ lib/ui/screens/BookClub/club_list_tab.dart
@@ -1,10 +1,46 @@
+import 'package:book_ganga/config/book_ganga.dart';
+import 'package:book_ganga/ui/widgets/my_input_field.dart';
+import 'package:book_ganga/ui/widgets/profile_avatar.dart';
 import 'package:flutter/material.dart';
 
 class ClubList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Club List View'),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+          child: MyInputField(
+            hintText: 'Search here...',
+          ),
+        ),
+        Expanded(
+          child: Container(
+            margin: const EdgeInsets.all(10.0),
+            child: ListView.builder(
+                itemCount: 25,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: ProfileAvatar(
+                            imageUrl:
+                                'https://www.vit.edu/images/Technical_chapter/trf-logo.jpg',
+                            radius: 25,
+                          ),
+                          title: Text('TRF Book Club'),
+                          subtitle: Text(
+                              'Official book club of The Robotics Forum, VIT, Pune'),
+                        ),
+                        const Divider(),
+                      ],
+                    ),
+                  );
+                }),
+          ),
+        )
+      ],
     );
   }
 }
